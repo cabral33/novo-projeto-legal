4f820c2bd6286e13be646388ad8214ea3e1054a1
lucas simplicio <lsc.hef@uea.edu.br>
2019-12-14 13:45:53 -0400

    initial

--- app/src/main/java/livro/com/br/MainActivity.java
+++ app/src/main/java/livro/com/br/MainActivity.java
@@ -1,16 +1,30 @@
 package livro.com.br;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText etPeso;
+    private EditText etAltura;
+    private Button btCalcular;
+    private Button btLimpar;
+    private TextView tvResul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etPeso = (EditText) findViewById(R.id.etPeso);
 
     }
 }
