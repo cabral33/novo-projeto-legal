1d644eeafa97d13a79f803ddd978033db22538cd
lucas simplicio <lsc.hef@uea.edu.br>
2019-12-14 14:13:04 -0400

    erro removido

--- app/src/main/java/livro/com/br/MainActivity.java
+++ app/src/main/java/livro/com/br/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etPeso = (EditText) findViewById(R.id.etPeso);
+        //etPeso = (EditText) findViewById(R.id.etPeso);
 
     }
 }
